9bd22be8b30dc6a4877bbfb5ea7a9911513f594c
onielrosario <onielrosario@pursuit.org>
2018-10-05 15:20:41 -0400

    initial commit

---
+++ anotherpractice/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  anotherpractice
+//
+//  Created by Oniel Rosario on 10/5/18.
+//  Copyright © 2018 Oniel Rosario. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//this is a practice test yeahhhh
+
